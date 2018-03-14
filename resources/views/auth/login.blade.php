857105a957d7cf2df821e07188d3db31b2bcfba5
Jorik de Boer <jorik_88@hotmail.com>
2018-03-14 16:43:29 +0100

    Patient-1 setup login structure episode VI: strong validation rules password and postalcode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -269,7 +269,7 @@
 
                         <!-- Register form password-->
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Wachtwoord') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Wachtwoord (minimaal 6 tekens: 3 kleine of hoofdletters, 1 cijfer en 1 speciaal teken)') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
