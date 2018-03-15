5e1b5a742f37ddae29879244cc92fb1c1b0e2855
Jorik de Boer <jorik_88@hotmail.com>
2018-03-15 13:13:29 +0100

    Patient-2 Able to edit account data Episode II: data of specific user is visible on account page

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -1,13 +1,25 @@
 <?php
 
+// Controller of the account section
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class AccountController extends Controller
 {
-    public function index()
+    public function index(User $user)
     {
-        return view('account');
+        $userid = Auth::id();
+		$users = DB::table('users')->where ('id', $userid)->get();
+        return view('accounts.index', compact('users'));
     }  
+    
+    public function show(User $user)
+	{
+        $userid = Auth::id();
+		$users = DB::table('users')->where ('id', $userid)->get();
+		return view('accounts.account', compact('users'));
+	}
 }
---
+++ resources/views/accounts/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Account</div>
+                
+                    @guest
+                    <!-- Show not logged in screen -->
+                    <div class="col-md-6">
+                        <label >{{ __('Please log in to see your account data.') }}</label>
+                    </div>
+                
+                    @else
+                    <!-- Show users account data -->
+                        @foreach ($users as $user) 
+                            @include('accounts.account')
+                        @endforeach
+                    @endguest
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
