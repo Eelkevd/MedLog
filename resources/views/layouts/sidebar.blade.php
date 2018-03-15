fc4be42ceedff9186109f860340c2f87a70c8081
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-03-15 14:32:15 +0100

    no icons

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,40 +7,26 @@
 
      <!-- Sidebar Links -->
      <ul class="list-unstyled components">
-        <li class="active">
-            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
-                <span class="glyphicon glyphicon-home"></span>`
-                Home
-            </a>
-        </li>
-        <li>
-             <a href="#">
-                 <span class="glyphicon glyphicon-briefcase"></span>`
-                 About
-             </a>
-        </li>
-        <li>
-             <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">
-                 <span class="glyphicon glyphicon-duplicate"></span>`
-                 Pages
-             </a>
-             <ul class="collapse list-unstyled" id="pageSubmenu">
-                 <li><a href="#">Page 1</a></li>
-                 <li><a href="#">Page 2</a></li>
-                 <li><a href="#">Page 3</a></li>
+         <li class="active"><a href="#">Home</a></li>
+         <li><a href="#">About</a></li>
+
+         <li><!-- Link with dropdown items -->
+             <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
+               Pages</a>
+             <ul class="collapse list-unstyled" id="homeSubmenu">
+                 <li><a href="#">Page</a></li>
+                 <li><a href="#">Page</a></li>
+                 <li><a href="#">Page</a></li>
              </ul>
-         </li>
-         <li>
-             <a href="#">
-                 <span class="glyphicon glyphicon-link"></span>`
-                 Portfolio
-             </a>
-         </li>
-       <li>
-           <a href="#">
-               <span class="glyphicon glyphicon-send"></span>`
-               Contact
-           </a>
-       </li>
+
+         <li><a href="#">Portfolio</a></li>
+         <li><a href="#">Contact</a></li>
+    </ul>
+    <!-- buttons -->
+    <ul class="list-unstyled CTAs">
+        <!-- white button -->
+        <li><a href="#" class="download">Back</a></li>
+        <!-- blue button -->
+        <li><a href="#" class="article">About</a></li>
     </ul>
   </nav>
