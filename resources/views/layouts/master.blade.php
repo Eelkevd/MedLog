f1110509e0349da9a41555d737b304364ddca84f
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-03-15 16:47:43 +0100

    Patient-5: added option to select an illness for a entry. Also added option that allows multiple symptomes to one entry. Modded and created migrations

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <meta name="csrf-token" content="{{csrf_token()}}">
+<<<<<<< HEAD
 
     <!-- Bootstrap CSS CDN-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -56,6 +57,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!-- Custom Scroller Js CDN -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+=======
+>>>>>>> Patient-5: added option to select an illness for a entry. Also added option that allows multiple symptomes to one entry. Modded and created migrations
 
     <script type="text/javascript">
         $(document).ready(function () {
@@ -76,5 +79,52 @@
         });
      </script>
 
+<<<<<<< HEAD
   </body>
 </html>
+=======
+</html>
+
+<!-- <script>
+	
+	$('#btnIllness').on("click", function () {
+
+		
+    // evt.preventDefault();
+
+    var illness = $('#illness').val();
+    // alert(items);
+    // var data = { 
+    // 			// "_token": "{{ csrf_token() }}", 
+    // 			type: type, 
+    // 			items: items
+    // 		};
+
+
+
+    $.ajaxSetup({
+	    headers: {
+	        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+	    }
+	})
+
+    $.ajax({
+        url: "/entries/create_illness",
+        type: "POST",
+        // data: { CSRF: getCSRFTokenValue(JSON.stringify(illness))},
+        data: {
+		        "_token": "{{ csrf_token() }}",
+		        "illness": illness
+        },
+        cache: false,
+        contentType: 'application/json; charset=utf-8',
+        processData: false,
+        success: function (response)
+        {
+            console.log(response);
+        }
+    });
+});
+
+</script> -->
+>>>>>>> Patient-5: added option to select an illness for a entry. Also added option that allows multiple symptomes to one entry. Modded and created migrations
