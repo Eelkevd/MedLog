84a32449f750e561b1d2940c1b0e85024a2105ef
Jorik de Boer <jorik_88@hotmail.com>
2018-03-16 13:24:25 +0100

    bugfixes aboutuspage

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -9,8 +9,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">About us</div>
-
-                <div class="card-body">
+                    <div class="card-body">
                     
                     <b>Over MedLog</b><br>
                     MedLog is jouw online medisch dagboek dat jou een overzicht geeft van jouw ziektes en bijbehorende symptomen, je een kalender geeft met jouw afspraken en medische geschiedenis en je een schema geeft van je medicijnen en hulpmiddelen. Geef je arts snel en gemakkelijk een uittreksel van je medische veranderingen en vergeet nooit meer cruciale medische informatie.<br><br>
@@ -19,7 +18,7 @@
                     
                     <b>Ons team</b>
                     <ul>
-                    <li>Eelke van dijk</li>
+                    <li>Eelke van Dijk</li>
                     <li>Esmeralda Tijhoff</li>
                     <li>Jorik de Boer</li>  
                     </ul>
@@ -48,7 +47,7 @@
                     <hr>
                     <b>Privacy-statement</b><br>
                     Persoonlijke gegevens die je via de site hebt ingevoerd, worden opgenomen in een gegevensbestand. Wij proberen de persoonlijke gegevens optimaal te beschermen bij het opslaan van de gegevens in onze database, al is 100 procent veilige opslag van de persoonlijke gegevens helaas onhaalbaar. De persoonlijke gegevens worden voor geen andere doeleinden gebruikt dan de gebruiker een overzicht te geven van zijn of haar eigen ingevoerde informatie. Naast door jou verstrekte gegevens worden ook gegevens verzameld met betrekking tot het bezoek van onze websites. Deze gegevens worden enerzijds gebruikt voor anoniem, statistisch onderzoek en anderzijds om de websites zoveel mogelijk op de voorkeuren van onze bezoeker af te stemmen. Dit laatste gebeurt onder meer door het gebruik van cookies. Je kunt je browser zo instellen dat het gebruik van cookies niet mogelijk is. Voor analyse-doeleinden maken we gebruik van Google Analytics-cookies.   
-                </div>
+                    </div>
             </div>
         </div>
     </div>
