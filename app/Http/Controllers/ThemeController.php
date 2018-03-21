c50e784bb0c9226410fde1e92b407de954c881ae
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-03-21 15:54:00 +0100

    ThemeController merge problem after reboot

---
+++ app/Http/Controllers/ThemeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use Illuminate\Http\Request;
+
+
+class ThemeController extends Controller
+{
+
+  public function update(Request $request)
+  {
+    $id = Auth::id();
+    $user = User::findOrFail($id);
+    User::where('id', $id)->update(['theme' => 'NULL' ]);
+
+    return back()
+    ->with('succes', 'Thema geactiveerd');
+  }
+
+  public function update_contrast(Request $request)
+  {
+    $id = Auth::id();
+    $user = User::findOrFail($id);
+    User::where('id', $id)->update(['theme' => 'contrast' ]);
+
+    return back()
+    ->with('succes', 'Thema geactiveerd');
+  }
+
+  public function update_vrolijk(Request $request)
+  {
+    $id = Auth::id();
+    $user = User::findOrFail($id);
+    User::where('id', $id)->update(['theme' => 'vrolijk' ]);
+
+    return back()
+    ->with('succes', 'Thema geactiveerd');
+  }
+
+}
