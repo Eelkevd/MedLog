2adef647456946e9521ede4a6127035107aa824d
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-03-21 11:20:58 +0100

    small merge fixes for jQuery

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,8 @@
   	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+<!-- <script type="text/javascript" src="<?php echo asset('assets/js/jquery.min.js'); ?>"></script> -->
     <meta name="csrf-token" content="{{csrf_token()}}">
 
     <!-- Bootstrap CSS CDN-->
@@ -86,46 +87,3 @@
 
   </body>
 </html>
-
-<!-- <script>
-
-	$('#btnIllness').on("click", function () {
-
-
-    // evt.preventDefault();
-
-    var illness = $('#illness').val();
-    // alert(items);
-    // var data = {
-    // 			// "_token": "{{ csrf_token() }}",
-    // 			type: type,
-    // 			items: items
-    // 		};
-
-
-
-    $.ajaxSetup({
-	    headers: {
-	        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-	    }
-	})
-
-    $.ajax({
-        url: "/entries/create_illness",
-        type: "POST",
-        // data: { CSRF: getCSRFTokenValue(JSON.stringify(illness))},
-        data: {
-		        "_token": "{{ csrf_token() }}",
-		        "illness": illness
-        },
-        cache: false,
-        contentType: 'application/json; charset=utf-8',
-        processData: false,
-        success: function (response)
-        {
-            console.log(response);
-        }
-    });
-});
-
-</script> -->
