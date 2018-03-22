b365f883e91b38409ab4b964a95092eaf53b1adc
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-03-22 15:22:45 +0100

    solved mergeconflicts

--- resources/views/homepage/mycalendar.blade.php
+++ resources/views/homepage/mycalendar.blade.php
@@ -20,7 +20,6 @@
         <html lang="en">
         <head>
              <script src="{{asset('js/app.js')}}"></script>
-<!--             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> -->
             <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
             <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css' />
