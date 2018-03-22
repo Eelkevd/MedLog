8b0e071c3333463bded652d8ef99510c65b19bb9
Jorik de Boer <jorik_88@hotmail.com>
2018-03-22 14:56:52 +0100

    flavicon test

--- resources/views/layouts/htmlheader_index.blade.php
+++ resources/views/layouts/htmlheader_index.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <!-- Our own flavicon -->
+    <link rel="icon" type="img/ico" href="img/favicon.ico"/>
     <!-- Bootstrap CSS CDN-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Styles van Laravel -->
