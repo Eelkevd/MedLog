0601f0c04843f0860f9fead1d6b9a70a6e1139fb
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-03-26 13:01:21 +0200

    VerifyController test

--- app/Http/Controllers/VerifyController.php
+++ app/Http/Controllers/VerifyController.php
@@ -25,6 +25,12 @@ class VerifyController extends Controller
         $diary = new Diary(['user_id' => $user_id]);
         $diary->save();
 
+
+        $user_id = $verifiedUser->id;
+        dd($user_id);
+
+        $diary = Diary::create('id_user', $user_id );
+
         return redirect('/home')
         ->with('succes', 'Dagboek geactiveerd');
     }
