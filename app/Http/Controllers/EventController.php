2ef4e97563a53b6b7bf01e41d160e33f513154d4
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-03-27 20:39:56 +0200

    all functioning database, controllers and models

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -43,7 +43,11 @@ class EventController extends Controller
     // adds new event to the event database to show later in calendar
     public function store(Request $request)
     {
-        $id = Auth::id();
+        // add the user_id to the request array
+  			$user_id = Auth::id();
+  			$request->request->add(['user_id' => $user_id]);
+
+        // send the request information through the model to store
         Event::create($request->all());
         $events = [];
         $request->validate([
