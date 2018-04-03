b13ad2231a9a6b132a7129a3552ae6736c53bb5d
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-04-03 22:06:52 +0200

    working query to store reader if reader is alraady a user, AND to make new user if reader is not registered yet

--- app/Http/Controllers/PermissionsController.php
+++ app/Http/Controllers/PermissionsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use Auth;
+use App\User;
+use App\Reader;
 
 use Illuminate\Http\Request;
 
@@ -37,18 +39,73 @@ class PermissionsController extends Controller
 		// Check if user is logged in
 		if (Auth::check())
 		{
-
 			// find the corresponding user
 			$user = Auth::user();
-			// add the user in the request array
-      $request->request->add(['user_id' => $user->id]);
-      // add the entry into the tabel entries
-			$permission = Reader::create(request([
-				'user_id',
-				'reader_id',
-			]));
+      $diary = $user->diary->id;
+      // check if the emailadres already excists in users
+      $email = $request->email_reader;
+      // make the request array
+      $request->request->add([
+        'timeframe' => 24,
+        'diary_id' => $diary,
+        'email' => $email,
+        'password' => 'password'
+      ]);
 
-			return redirect ('permissions');
-    }
+      // check if the reader already excists
+      $reader = User::where('email', $email)
+      ->firstOrFail();
+      $reader_id = $reader->id;
+      // handle the emails
+      if(!empty($reader_id))
+      {
+        // check if reader is 'hulpverlener'
+        // if so : send email to invite to log in
+        // if not : give error , ask for different emailadres
+
+        $request->request->add([
+          'user_id' => $reader_id,
+        ]);
+        // add the entry into the tabel READERS entries
+        $permission = Reader::create(request([
+          'user_id',
+          'timeframe',
+          'diary_id',
+          'email',
+          'password'
+        ]));
+
+        return redirect ('permissions')
+        ->with('succes', 'Nieuwe lezer toegevoegd');
+
+      }
+      else
+      {
+        // reader is new, send invitation to register
+
+        // add a new user with this email adress to the table users
+        // add him to to pivettable roles as hulpverlener
+        $userName = Str::random(6);
+        $wachtwoord = Str::random(8);
+
+        $user = User::create([
+              'username' => $userName,
+              'email' => $email,
+              'password' => Hash::make($wachtwoord),
+              'verifyToken' => Str::random(40),
+          ]);
+          // Attach th role of the user in the pivot table role_user
+          $user->roles()->attach($data['role']);
+          // Send an email with a verification link which redirects using the token
+          $user->sendVerificationMail();
+          return $user;
+
+
+        return redirect ('/')
+        ->with('succes', 'Inlog voor lezer aangemaakt en verzonden');
+
+      }
+
   }
+}
 }
--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -39,6 +39,15 @@
                     @else
 
                     <div class="card-body">
+                      <div class="alert alert-primary">
+                      U kunt iemand tijdelijk leesrechten geven over uw medisch dagboek. Zo kunt u uw dokter bijvoorbeeld
+                        laten meekijken, of uw mantelzorger.<br />
+                      </div>
+                      @if(Session::has('succes'))
+                      <div class="alert alert-success">
+                          {{ Session::get('succes') }}
+                        </div>
+                      @endif
                       <table class="table table-striped">
                           <thead>
                             <tr>
@@ -48,28 +57,54 @@
                           </thead>
                           <tbody>
 
-                            <!-- Show reader name -->
-                            @foreach($permissions as $permission)
+                            @if (!empty($permissions))
+                              <!-- Show reader name -->
+                              @foreach($permissions as $permission)
 
-                            <tr>
-                                <td><b>{{ __('Naam: ') }}</b></td>
-                                <td>{{ $permission -> user -> firstname }}
-                                  @if (!empty( $permission -> user -> middlename ))
-                                    {{ $permission -> user -> middlename }}
-                                  @endif
-                                     {{ $permission -> user -> lastname }}</td>
-                                <td>{{ $permission -> user -> email }}</td>
-                                <td>
-                                  <a href="permissions/remove">
-                                    <span class="glyphicon glyphicon-trash">
-                                      <u>verwijder</ul>
-                                    </span>
-                                  </a>
-                                </td>
-                            </tr>
-                            @endforeach
+                              <tr>
+                                  <td><b>{{ __('Naam: ') }}</b></td>
+                                  <td>{{ $permission -> user -> firstname }}
+                                    @if (!empty( $permission -> user -> middlename ))
+                                      {{ $permission -> user -> middlename }}
+                                    @endif
+                                       {{ $permission -> user -> lastname }}</td>
+                                  <td>{{ $permission -> user -> email }}</td>
+                                  <td>
+                                    <a href="permissions/remove">
+                                      <span class="glyphicon glyphicon-trash">
+                                        <u>verwijder</ul>
+                                      </span>
+                                    </a>
+                                  </td>
+                              </tr>
+                              @endforeach
+                            @else
+                             <tr>
+                               <td>U heeft nog geen lezers</td>
+                             </tr>
+                            @endif
+                            </tbody>
+                         </table>
+                     </div>
+                   </div>
 
-                       </tabla>
+                   <br />
+                   <div class="card">
+                     <!-- form for sadding readers -->
+                     	<div class="card-header">
+                     		<strong>Voeg een nieuwe lezer toe</strong><br />
+                        Geef het emailadres op van de persoon die u als lezer wilt toevoegen.
+                     	</div>
+
+                     	<div class="card-body">
+                     		<form method="POST" action="/permissions/givepermission">
+                     			{{ csrf_field() }}
+                     		    <div>
+                     				<input type="text" name="email_reader" placeholder="emailadres van uw lezer" style="width:400px;">
+                     				<input type="submit" align="center" class="btn btn-primary" value="voeg lezer toe">
+                     			</div>
+                     		</form>
+                     	</div>
                      </div>
 
                  @endif
