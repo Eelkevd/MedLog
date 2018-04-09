336241bf195919817f1f5fdceac67304ecf6fe59
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-04-09 14:04:28 +0200

    Added most recent entries on overview page

--- app/Http/Controllers/OverviewController.php
+++ app/Http/Controllers/OverviewController.php
@@ -36,7 +36,7 @@ class OverviewController extends Controller
         $sortillness = $user->diary->entries()->where('illness', 'LIKE', '%' . $sortword . '%')->orderBy('timespan_date', 'DESC')->get();
         $sortintensity = $user->diary->entries()->where('intensity', 'LIKE', '%' . $sortword . '%')->orderBy('timespan_date', 'DESC')->get();
         $illnesses = $user->diary->illnesses->sortByDesc('timespan_date');
-        $entries = $user->diary->entries()->orderBy('timespan_date', 'DESC')->paginate(5);
+        $entries = $user->diary->entries()->orderBy('timespan_date', 'DESC')->orderBy('timespan_time', 'DEC')->paginate(5);
         return view('overview', compact('sortillness','search', 'illnesses', 'sortintensity', 'entries'));
       }
       else
